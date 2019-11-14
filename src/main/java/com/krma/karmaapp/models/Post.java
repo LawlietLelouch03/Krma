895898ebedddcc2c5d79ee4631ffe243b5a5eb34
soporte <pachilord3@gmail.com>
2019-11-14 17:30:34 -0600

    Merge origin/ErickBranch into VincentBranch
    Conflicts:
    	src/main/resources/hibernate.cfg.xml

--- src/main/java/com/krma/karmaapp/models/Post.java
+++ src/main/java/com/krma/karmaapp/models/Post.java
@@ -6,14 +6,11 @@
 package com.krma.karmaapp.models;
 
 import java.sql.Date;
-import java.util.List;
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
-import javax.persistence.ManyToMany;
-import javax.persistence.OneToMany;
 import javax.persistence.OneToOne;
 
 /**
